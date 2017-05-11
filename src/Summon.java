6a24b359ef6887380766c49d633d47e5fc489847
orthu <orthu@DESKTOP-JIJE882>
2017-05-11 14:08:03 -0600

    Bug is Fixed, ready for main

--- src/Summon.java
+++ src/Summon.java
@@ -4,6 +4,9 @@ public class Summon {
 	public static void main(String[] args) {
 		System.out.println("Initial build 13:24 11 May 2017");
 		System.out.println("Bug fix for null");
+		
+		
+		System.out.println("Yea, all fixed, Merging with main");
 	}
 
 }
